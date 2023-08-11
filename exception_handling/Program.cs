eecd6d3cc436d71ebe8474cf3a634d29c6c8a6a8
Miray Mutlu <miraymutlu@Mirays-MacBook-Air.local>
2023-08-11 21:08:40 +0300

    exception handling added

---
+++ exception_handling/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace exception_handling;
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int result = 10 / num; // Potential division by zero
+
+            Console.WriteLine("Result: " + result);
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Invalid input format: " + ex.Message);
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Division by zero error: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Execution completed.");
+        }
+
+        Console.ReadLine();
+    }
+}
