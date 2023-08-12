f2f7f099f8fedcb8fecc2161c703cd6aaace7c08
Miray Mutlu <miraymutlu@Mirays-MacBook-Air.local>
2023-08-12 14:19:05 +0300

    for loops added

---
+++ for_loops/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace for_loops;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Counting from 1 to " + n + ":");
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write(i + " ");
+        }
+
+        Console.WriteLine("\nLoop completed.");
+
+        Console.ReadLine();
+    }
+}
