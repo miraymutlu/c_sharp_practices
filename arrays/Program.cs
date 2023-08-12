49001e3007b16417af6dc835f21177df480a7230
Miray Mutlu <miraymutlu@Mirays-MacBook-Air.local>
2023-08-12 15:01:06 +0300

    arrays added

---
+++ arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace arrays;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creating and initializing an array of integers
+        int[] numbers = new int[5]; // Creating an array of size 5
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+        numbers[3] = 40;
+        numbers[4] = 50;
+
+        Console.WriteLine("Array of numbers:");
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine("Element at index " + i + ": " + numbers[i]);
+        }
+
+        Console.WriteLine();
+
+        // Creating and initializing an array of strings using array initializer
+        string[] fruits = { "Apple", "Banana", "Orange", "Grapes" };
+
+        Console.WriteLine("Array of fruits:");
+        foreach (string fruit in fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+
+        Console.ReadLine();
+    }
+}
