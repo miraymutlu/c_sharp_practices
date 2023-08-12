3b5e5c12709620a5da4199990ab99fe8b9fbdc11
Miray Mutlu <miraymutlu@Mirays-MacBook-Air.local>
2023-08-12 13:59:25 +0300

    if-else added

---
+++ if_else/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace if_else;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your age: ");
+        int age = int.Parse(Console.ReadLine());
+
+        if (age >= 18)
+        {
+            Console.WriteLine("You are an adult.");
+        }
+        else
+        {
+            Console.WriteLine("You are not yet an adult.");
+        }
+
+        Console.ReadLine();
+    }
+}
