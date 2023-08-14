3de69d8ac91d8639550031747aeeda03db482c2e
Miray Mutlu <miraymutlu@Mirays-MacBook-Air.local>
2023-08-14 13:49:33 +0300

    recursive and extension methods added

---
+++ recursive_and_extension_methods/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace RecursiveAndExtensionMethods
+{
+    public static class StringExtensions
+    {
+        public static string ReverseString(this string input)
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+
+    class Program
+    {
+        // Recursive method to calculate the factorial of a number
+        static int Factorial(int n)
+        {
+            if (n == 0 || n == 1)
+                return 1;
+            else
+                return n * Factorial(n - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            // Recursive method example
+            int num = 5;
+            int factorial = Factorial(num);
+            Console.WriteLine($"Factorial of {num} is {factorial}");
+
+            // Extension method example
+            string originalString = "Hello, world!";
+            string reversedString = originalString.ReverseString();
+            Console.WriteLine($"Original string: {originalString}");
+            Console.WriteLine($"Reversed string: {reversedString}");
+
+            Console.ReadLine();
+        }
+    }
+}
