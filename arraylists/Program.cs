f514d5357e81ed2d7870b0725c787c093b7e1760
Miray Mutlu <miraymutlu@Mirays-MacBook-Air.local>
2023-08-14 20:47:10 +0300

    arraylists added

---
+++ arraylists/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+
+namespace arraylists;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create an ArrayList
+        ArrayList list = new ArrayList();
+
+        // Adding elements to the ArrayList
+        list.Add(42);
+        list.Add("Hello");
+        list.Add(3.14);
+
+        Console.WriteLine("ArrayList elements:");
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+
+        // Removing an element by value
+        list.Remove("Hello");
+
+        Console.WriteLine("\nAfter removing 'Hello':");
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+
+        // Checking if an element exists
+        bool contains42 = list.Contains(42);
+        Console.WriteLine($"\nContains 42: {contains42}");
+
+        // Finding the index of an element
+        int indexOf314 = list.IndexOf(3.14);
+        Console.WriteLine($"Index of 3.14: {indexOf314}");
+
+        // Clearing the ArrayList
+        list.Clear();
+
+        Console.WriteLine("\nAfter clearing:");
+        Console.WriteLine($"Count: {list.Count}");
+
+        Console.ReadLine();
+    }
+}
