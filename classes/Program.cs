db047336eee758057537de13638a689e33fb2d7b
Miray Mutlu <miraymutlu@Mirays-MacBook-Air.local>
2023-08-16 16:42:56 +0300

    classes added

---
+++ classes/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Person
+{
+    // Fields
+    private string name;
+    private int age;
+
+    // Properties
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+
+    // Constructor
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Method
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name: {name}, Age: {age}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creating instances (objects) of the Person class
+        Person person1 = new Person("Alice", 25);
+        Person person2 = new Person("Bob", 30);
+
+        // Using properties to set values
+        person1.Name = "Carol";
+        person2.Age = 35;
+
+        // Calling methods on instances
+        person1.DisplayInfo();
+        person2.DisplayInfo();
+    }
+}
