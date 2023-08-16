7c152bbe7e5168b6af2f2ad61be44f8d6be4e6d1
Miray Mutlu <miraymutlu@Mirays-MacBook-Air.local>
2023-08-16 16:34:33 +0300

    hw2 completed

---
+++ hw2/hw2_3/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace hw2_3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Write a sentences: ");
+        string input = Console.ReadLine();
+        List<char> list = new List<char>();
+        for(int i=0; i<input.Length; i++){
+           input.ToLower();
+           if(input[i]=='a'||input[i]=='e'||input[i]=='i'||input[i]=='ı'||input[i]=='o'||input[i]=='ö'||input[i]=='u'||input[i]=='ü'){
+                list.Add(input[i]);
+           } 
+        }
+        foreach(char letter in list){
+            Console.WriteLine(letter);
+        }
+    }
+}
