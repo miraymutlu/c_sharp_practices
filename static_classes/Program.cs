8918e80b31a98ab8472a30857a20b5248688e717
Miray Mutlu <miraymutlu@Mirays-MacBook-Air.local>
2023-08-17 14:09:59 +0300

    static classes added

---
+++ static_classes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+// Static class with static members
+static class MathUtility
+{
+    // Static field
+    public static double Pi = 3.14159;
+
+    // Static method
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    // Static property
+    public static int MultiplyByTwo { get; set; }
+
+    // Static constructor (optional)
+    static MathUtility()
+    {
+        MultiplyByTwo = 2;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Accessing static field
+        Console.WriteLine("Pi: " + MathUtility.Pi);
+
+        // Accessing static method
+        int sum = MathUtility.Add(5, 7);
+        Console.WriteLine("Sum: " + sum);
+
+        // Accessing static property
+        int result = 10 * MathUtility.MultiplyByTwo;
+        Console.WriteLine("Result: " + result);
+    }
+}
