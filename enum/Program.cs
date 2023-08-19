65fdcf9ca12cfb75dcde45b7e9eed27ca7ea6700
Miray Mutlu <miraymutlu@Mirays-MacBook-Air.local>
2023-08-19 12:20:17 +0300

    enum added

---
+++ enum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+// Define an enum named DaysOfWeek
+enum DaysOfWeek
+{
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Using enum values
+        DaysOfWeek today = DaysOfWeek.Wednesday;
+
+        Console.WriteLine("Today is: " + today);
+
+        // Enum comparison
+        if (today == DaysOfWeek.Wednesday)
+        {
+            Console.WriteLine("It's midweek!");
+        }
+
+        // Enum iteration
+        Console.WriteLine("Days of the week:");
+        foreach (DaysOfWeek day in Enum.GetValues(typeof(DaysOfWeek)))
+        {
+            Console.WriteLine(day);
+        }
+    }
+}
